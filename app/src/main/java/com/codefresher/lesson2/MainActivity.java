2291582d8a3a3c4eb93d5b2035ba6e7651078314
Duy Le <lehongduy.bk@gmail.com>
2022-05-16 20:02:58 +0700

    test add library

--- app/src/main/java/com/codefresher/lesson2/MainActivity.java
+++ app/src/main/java/com/codefresher/lesson2/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity { // update somethings
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int idRes = R.mipmap.ic_launcher;
+
+        int resId2 = R.drawable.ic_launcher_background;
     }
 }
